73d6efb3319af67eccd3cc9313d65fed98666d65
AlexRosly <rosly.a777@gmail.com>
2022-08-18 21:35:18 +0300

    chenge response name

--- controllers/country/getAllCountry.js
+++ controllers/country/getAllCountry.js
@@ -1,12 +1,12 @@
 const { Country } = require("../../models");
 
 const getAllCountries = async (req, res) => {
-  const contries = await Country.find({});
+  const countries = await Country.find({});
   res.json({
     status: "success",
     code: 200,
     data: {
-      contries,
+      countries,
     },
   });
 };
