c04708afc20848e2ebf24156518143b3bb4b925f
AlexRosly <rosly.a777@gmail.com>
2023-09-23 22:37:37 +0300

    change logic to currency

--- controllers/currency/getCurrency.js
+++ controllers/currency/getCurrency.js
@@ -5,28 +5,39 @@ const getCurrency = async (req, res) => {
   const baseCurrency = "USD";
   //find currency from DB
   const getCurrency = await Currency.find();
+  const currencies = getCurrency[0];
+
   // time now
   const date = new Date().getTime();
   const getTimeInSeconds = Math.floor(date / 1000);
 
-  //check if date > date from DB, we have been make next steps:
-  //remove present currency from DB
-  //make new request to Api services
-  //add new rate to DB
+  //function that delete all documents in DB
 
-  if (getTimeInSeconds > getCurrency[0].time_next_update_unix) {
-    const deleteOldCurrency = await Currency.deleteOne({
-      _id: getCurrency[0].id,
-    });
+  const deleteCurrency = async () => {
+    const result = await Currency.deleteMany({});
+    return result;
+  };
+
+  //function that get new Exchange rate
+
+  const getAviableCurrency = async (baseCurrency) => {
+    const getNewCurrency = await currency(baseCurrency);
+    return getNewCurrency;
+  };
+
+  //check in DB. If in DB wrong data. make new request in API and add data to DB
+
+  if (!currencies.time_next_update_unix) {
+    const deleteOldCurrency = await deleteCurrency();
 
     if (!deleteOldCurrency) {
       return res.status(404).json({
-        message: "Exchange rate service is unavailable. You can try later",
+        message: "Oops something wrong. You can try later",
         code: 404,
       });
     }
 
-    const getNewCurrency = await currency(baseCurrency);
+    const getNewCurrency = await getAviableCurrency(baseCurrency);
 
     if (!getNewCurrency) {
       return res.status(404).json({
@@ -35,11 +46,41 @@ const getCurrency = async (req, res) => {
       });
     }
 
-    const addCurrency = await Currency.create({ ...getNewCurrency });
+    await Currency.create({ ...getNewCurrency });
+    const addCurrency = await Currency.find();
+    return res.status(200).json(addCurrency[0]);
+  }
+
+  //check if date > date from DB, we have been make next steps:
+  //remove present currency from DB
+  //make new request to Api services
+  //add new rate to DB
+
+  if (getTimeInSeconds > currencies.time_next_update_unix) {
+    const deleteOldCurrency = await deleteCurrency();
+
+    if (!deleteOldCurrency) {
+      return res.status(404).json({
+        message: "Oops something wrong. You can try later",
+        code: 404,
+      });
+    }
+
+    const getNewCurrency = await getAviableCurrency(baseCurrency);
+
+    if (!getNewCurrency) {
+      return res.status(404).json({
+        message: "Exchange rate service is unavailable. You can try later",
+        code: 404,
+      });
+    }
+
+    await Currency.create({ ...getNewCurrency });
+    const addCurrency = await Currency.find();
 
     return res.status(200).json(addCurrency[0]);
   } else {
-    return res.status(200).json(getCurrency[0]);
+    return res.status(200).json(currencies);
   }
 };
 
