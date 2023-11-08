d6ae7de2e37e8d526fdbb2187f9ad53cc168216b
AlexRosly <rosly.a777@gmail.com>
2023-11-08 14:19:18 +0200

    add getAllUser controller for admin

--- controllers/acsessToAdmin/statistic.js
+++ controllers/acsessToAdmin/statistic.js
@@ -8,24 +8,20 @@ const {
 } = require("../../models");
 
 const statistic = async (req, res) => {
-  //find amount of objects winth status "on verification"
-  const getHotels = await Hotels.find({ status: "on verification" });
-  const objectOnVerification = getHotels.length;
+  //find amount of objects
+  const getHotels = await Hotels.find({});
+  const amountOfObject = getHotels.length;
 
-  //find amount of objects witn status
-  const getActiveHotels = await Hotels.find({ status: "active" });
-  const activeObject = getActiveHotels.length;
-
-  //find amount of user winth status "active"
-  const getUser = await User.find({ status: "active" });
+  //find amount of user
+  const getUser = await User.find({});
   const users = getUser.length;
 
-  //find amount of user winth status "active"
-  const getHoteliers = await Hotelier.find({ status: "active" });
+  //find amount of user
+  const getHoteliers = await Hotelier.find({});
   const hoteliers = getHoteliers.length;
 
   //find amount of user winth status "active"
-  const getAgent = await Agent.find({ status: "active" });
+  const getAgent = await Agent.find({});
   const agents = getAgent.length;
 
   // bookineg options in active objects
@@ -34,15 +30,27 @@ const statistic = async (req, res) => {
   const amountOfBookingOption =
     getBookingOption.length + getBookingOptionHs.length;
 
+  if (
+    !amountOfObject &&
+    !users &&
+    !hoteliers &&
+    !agents &&
+    !amountOfBookingOption
+  ) {
+    return res.json({
+      code: 404,
+      message: "error",
+    });
+  }
+
   res.json({
     result: {
-      objectOnVerification,
+      amountOfObject,
       amount: {
         users,
         hoteliers,
         agents,
       },
-      activeObject,
       amountOfBookingOption,
     },
   });
