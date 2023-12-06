b424faa9d9a7d864c9cd10d423f07c2cf5f0b124
AlexRosly <rosly.a777@gmail.com>
2023-12-06 14:09:36 +0200

    change autodeploy setup

--- bin/server.js
+++ bin/server.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 const app = require("../app");
 
-const { DB_HOST, PORT = 5000 } = process.env;
+const { DB_HOST, APP_PORT = 5000 } = process.env;
 
 mongoose.set("strictQuery", false);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(PORT);
+    app.listen(APP_PORT);
     console.log("Database connection successful");
   })
   .catch((error) => {
